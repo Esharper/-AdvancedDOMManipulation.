f142cebcf91e8d94e59fe84fbcfd0d3bcb4c5b5f
Elgin <elginsharper87@gmail.com>
2023-12-02 22:49:49 -0600

    new buuton

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+document.addEventListener('DOMCONTENTLOADED', function () {
+const header=document.querySelector('header');
+console.log('Selected header:', header);
+
+});
+
+const listitems=document.querySelectorAll('li');
+
+console.log('List Items:', listItems);  
